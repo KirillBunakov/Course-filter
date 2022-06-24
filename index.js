281ae218d2e1a89cccfb91da12f78c637043daeb
KirillBunakov <98851772+KirillBunakov@users.noreply.github.com>
2022-06-24 19:40:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ let courses = [
   { name: "Courses in France", prices: [0, 630] },
 ];
 
-
 function filter(arr,prop,value1,value2) {
   let result = []
   copy = [...arr]
@@ -24,20 +23,20 @@ function filter(arr,prop,value1,value2) {
 console.log(filter(courses, 'prices', 0, 500));
 
 function render(arr) {
-  const list = document.querySelector('.courses-list')
+const list = document.querySelector('.courses-list')
   list.innerHTML = ''
 
-  const inpValue1 = document.getElementById('inp-value1').value,
+const inpValue1 = document.getElementById('inp-value1').value,
   inpValue2 = document.getElementById('inp-value2').value
 
-  let newArr = [...arr]
+let newArr = [...arr]
   if(inpValue1 && inpValue2) newArr = filter(newArr, 'prices', inpValue1,inpValue2)
   
 
-  for (const course of newArr) {
-    const li = document.createElement("li")
-    li.textContent = course.name + ', стоимость: от ' + course.prices[0] +' до ' + course.prices[1]
-    list.append(li)
+for (const course of newArr) {
+  const li = document.createElement("li")
+  li.textContent = course.name + ', стоимость: от ' + course.prices[0] +' до ' + course.prices[1]
+  list.append(li)
   }
 }
 
@@ -47,10 +46,6 @@ document.getElementById('filter-form').addEventListener('submit', function(event
 })
 
 render(courses)
-// Варианты цен (фильтры), которые ищет пользователь
-// let requiredRange1 = [null, 200];
-// let requiredRange2 = [100, 350];
-// let requiredRange3 = [200, null];
 
 
 
